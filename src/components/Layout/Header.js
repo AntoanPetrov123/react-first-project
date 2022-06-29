b241c7019108615e8505e9d493548aaa1e4b424e
Antoan Petrov <antoan999_kn@abv.bg>
2022-06-29 15:01:59 +0300

    init commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import backgroundImage from '../../assets/cars-background.png';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1 className={classes.head}>Change Your Car</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={backgroundImage} alt='background cars' />
+        </div>
+    </Fragment>;
+};
+
+export default Header;
