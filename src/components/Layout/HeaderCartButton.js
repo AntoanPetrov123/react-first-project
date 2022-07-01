1250309e74a329540b06f46a25f65c881c3a14b7
Antoan Petrov <antoan999_kn@abv.bg>
2022-07-01 12:18:39 +0300

    add effect on cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from '../../storage/cart-context';
 import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = props => {
 
+    const [btnHighlight, setBtnHighlight] = useState(false);
     const cartContext = useContext(CartContext);
 
     const numberOfCartItems = cartContext.items.length;
 
+    const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartContext.items.length === 0) {
+            return;
+        }
+        setBtnHighlight(true);
+
+        const timer = setTimeout(() => {
+            setBtnHighlight(false);
+        }, 300);
+
+        //clean up function 
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartContext.items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
