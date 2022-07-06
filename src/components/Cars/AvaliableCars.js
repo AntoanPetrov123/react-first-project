0b6196093db2e4f6aaf42ba26d18b3c11156797e
Antoan Petrov <antoan999_kn@abv.bg>
2022-07-06 11:23:57 +0300

    fetching cars data from db

--- src/components/Cars/AvaliableCars.js
+++ src/components/Cars/AvaliableCars.js
@@ -1,37 +1,34 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../UI/Card';
 import CarItem from './CarItem/CarItem';
 
 import classes from './AvaliableCars.module.css';
 
-const DUMMY_CARS = [
-    {
-        id: 'car1',
-        name: 'Mercedes',
-        description: 'Brand New Model 2022 CLS 8.0 BTurbo',
-        imageUrl: 'https://www.drivearabia.com/app/uploads/2021/04/mercedes-amg-cls-53-2021-4.jpg',
-        price: 153000.00,
-        inCart: false,
-    },
-    {
-        id: 'car2',
-        name: 'AUDI',
-        description: 'Audi S8 2019 from Germany 140000km, 260hp',
-        imageUrl: 'http://superkoli.net/wp-content/uploads/2019/07/Audi-S8-Official2.jpg',
-        price: 69000.00,
-        inCart: false,
-    },
-    {
-        id: 'car3',
-        name: 'Lamborgini',
-        description: 'Brand New Lambo, color: green, hp: 500, gear: automatic',
-        imageUrl: 'https://i.pinimg.com/originals/5a/70/fa/5a70fa3a41a543693f3d3ae018b82eab.jpg',
-        price: 1200000.00,
-        inCart: false,
-    },
-];
-
 const AvaliableCars = () => {
-    const carList = DUMMY_CARS.map((car) =>
+    const [cars, setCars] = useState([]);
+    useEffect(() => {
+        const fetchCars = async () => {
+            const response = await fetch('https://change-your-car-react-default-rtdb.firebaseio.com/cars.json');
+            const responseData = await response.json();
+
+            const loadedCars = [];
+
+            for (const key in responseData) {
+                loadedCars.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    imageUrl: responseData[key].imageUrl,
+                    price: responseData[key].price,
+                });
+            }
+            setCars(loadedCars);
+        };
+
+        fetchCars();
+    }, []);
+    const carList = cars.map((car) =>
         <CarItem
             key={car.id}
             id={car.id}
