a0b2f8fc5d5458f7c34b8e3b1f51c51a4854ad95
Antoan Petrov <antoan999_kn@abv.bg>
2022-07-07 12:30:08 +0300

    navigation and home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,17 @@
+import classes from './Home.module.css';
 const Home = () => {
-    return(
-        <h1>Welcome to Home Page!</h1>
+    return (
+        <section className={classes.home}>
+            <h1 className={classes.header}>Welcome <accent className={classes.accent}>to Our site</accent></h1>
+            <div className={classes.dividers}>
+                <hr className={classes['divider-one']} />
+                <hr className={classes['divider-two']} />
+            </div>
+            <p className={classes.text}>
+                Here you can find brand new or second hand cars.
+                This site is new, and we will be happy if you share it with friend!
+            </p>
+        </section>
     );
 };
 
