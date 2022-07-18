742c366f27860e2c1eaf10aa8ccf81b4969a8e5b
Antoan Petrov <antoan999_kn@abv.bg>
2022-07-18 13:10:50 +0300

    delete item from database

--- src/components/Authentication/Profile/CarItemProfile.js
+++ src/components/Authentication/Profile/CarItemProfile.js
@@ -1,10 +1,75 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './CarItemProfile.module.css';
 const CarItemProfile = props => {
 
     const price = `$${+props.price}`
 
-    console.log(props, 'props');
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    console.log(props.id, 'props');
+
+    const deleteButtonHandler = (postId) => {
+
+        const fetchCars = async () => {
+            const responsePosts = await fetch(`https://change-your-car-react-default-rtdb.firebaseio.com/cars/${postId}.json`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            if (!responsePosts.ok) {
+                throw new Error('Somthing is wrong!');
+            }
+        };
+
+        const fetchCarsInProfile = async () => {
+            const responsePostsInProfile = await fetch(`https://change-your-car-react-default-rtdb.firebaseio.com/users/${localStorage.userId}/posts.json`);
+            if (!responsePostsInProfile) {
+                console.log('empty');
+            }
+            const resData = await responsePostsInProfile.json();
+
+            const dataEntries = Object.entries(resData);
+        
+            let indexForDelete;
+
+            dataEntries.map(([k, v]) => v === postId ? indexForDelete = k : indexForDelete);
+
+            const responseCurrentPostInProfile = await fetch(`https://change-your-car-react-default-rtdb.firebaseio.com/users/${localStorage.userId}/posts/${indexForDelete}.json`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            if (!responseCurrentPostInProfile.ok) {
+                throw new Error('Somthing is wrong!');
+            }
+        };
+
+        //chech if we have any errors
+        fetchCars().catch((err) => {
+            setIsLoading(false);
+            setError(err.message);
+        });
+
+        fetchCarsInProfile().catch((err) => {
+            setIsLoading(false);
+            setError(err.message);
+        });
+    };
+
+    if (isLoading) {
+        return (
+            <div className={classes['spinner-container']} >
+                <div className={classes["loading-spinner"]}>
+                </div>
+            </div>
+        );
+    }
+    
 
     return (
         <li className={classes.car}>
@@ -17,10 +82,10 @@ const CarItemProfile = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div className={classes['details-btn']}>
-                    <Link to={`/edit/${props.id}`}>Edit</Link>
+                <Link to={`/edit/${props.id}`} params={{ name: props.name, imageUrl: props.imageUrl, description: props.description, price: price }}>Edit</Link>
             </div>
             <div>
-                <button  className={classes['delete-btn']}>Delete</button>
+                <button className={classes['delete-btn']} onClick={() => deleteButtonHandler(props.id)} >Delete</button>
             </div>
         </li>
     );
