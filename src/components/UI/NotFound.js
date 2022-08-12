1664fb1780475e14afe00d32fee3e997c50b0fe4
Antoan Petrov <72857425+AntoanPetrov123@users.noreply.github.com>
2022-08-12 10:22:54 +0300

    Update NotFound.js

--- src/components/UI/NotFound.js
+++ src/components/UI/NotFound.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import classes from './NotFound.module.css';
 
 const NotFound = () => {
@@ -5,6 +6,7 @@ const NotFound = () => {
     return (
         <div className={classes.centered}>
             <p>Page Not Found!</p>
+            <Link className={classes["go-home"]} to="/">Go home!</Link>
         </div>
     );
 };
